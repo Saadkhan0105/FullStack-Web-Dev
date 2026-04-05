ea0e06bda45e24ef9cb649de61a96a3467d3c119
Saadkhan0105 <saadkhan9560@gmail.com>
2026-04-05 15:52:45 +0530

    Operations in JS

---
+++ JavaScript/part1/logical.js
@@ -0,0 +1,13 @@
+// && -> and
+// || -> or
+// ! -> reverse
+
+let isLoggedIn = true;
+let isPaid = true;
+
+// console.log(isLoggedIn && isPaid);
+
+let isEmailuser = true;
+let isGoogleUser = false;
+
+console.log(isEmailuser || isGoogleUser);
